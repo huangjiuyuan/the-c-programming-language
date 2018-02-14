ec254325b22ba63b29049baaf8b8efdc4e3b0268
huangjiuyuan <jiuyuan.huang@daocloud.io>
2018-02-14 12:13:25 +0800

    Chapter 2 section 2.4 done

---
+++ chapter_2/2_04/declaration.c
@@ -0,0 +1,14 @@
+main()
+{
+    /* declarations */
+    int lower, upper, step;
+    char c, line[1000];
+
+    /* initializations */
+    char esc = '\\';
+    int i = 0;
+    float eps = 1.0e5;
+
+    const double e = 2.71828182845905;
+    const char msg[] = "warning";
+}
